607420e97af021064160ed400a80e9ff798758f7
Ryan <Ryan@DESKTOP-C4E4KQA>
2019-05-10 17:36:33 -0600

    Adding first switch

--- Section_3_Calculator/src/main/java/Controller/Controller.java
+++ Section_3_Calculator/src/main/java/Controller/Controller.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import lib.ConsoleIO;
+
 public class Controller 
 {
 
@@ -18,9 +20,40 @@ public class Controller
 	
 	public static void RunMainMenu()
 	{
+		int choice  = -1;
 		
+		choice = ConsoleIO.promptForInt("1: Simplifying Polynomials.\n2: Rational Functions. \nAnswer Here: ", 1, 3);
 		
+		switch (choice)
+		{
+			case 1: 
+				SimplifyingPolynomials();
+				break;
+				
+			case 2: 
+				RationalFunctions();
+				break;
+				
+			case 3: 
+				//TODO
+				break;
+				
+			default:
+				System.out.println("Error. In Menu One.");
+				break;
+		}
 		
 	}
+	
+	public static void SimplifyingPolynomials()
+	{
+		
+	}
+	
+	public static void RationalFunctions()
+	{
+		
+	}
+	
 
 }

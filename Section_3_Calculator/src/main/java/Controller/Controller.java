7ec06554625edfa45c14845b5326666cb49c1c84
Ryan <Ryan@DESKTOP-C4E4KQA>
2019-05-10 17:20:07 -0600

    first steps

---
+++ Section_3_Calculator/src/main/java/Controller/Controller.java
@@ -0,0 +1,26 @@
+package Controller;
+
+public class Controller 
+{
+
+	public static void main(String[] args) 
+	{
+		Run();
+
+	}
+	
+	public static void Run()
+	{
+		
+		RunMainMenu();
+		
+	}
+	
+	public static void RunMainMenu()
+	{
+		
+		
+		
+	}
+
+}

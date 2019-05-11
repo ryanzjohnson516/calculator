3260c7c1abcbfe13ccff1e4160dc3d5f89838adb
Lucas <lucasmcmanus21@gmail.com>
2019-05-10 18:35:00 -0600

    Built Most of Quadratic Class

--- Section_3_Calculator/src/main/java/models/Quadratic.java
+++ Section_3_Calculator/src/main/java/models/Quadratic.java
@@ -2,4 +2,39 @@ package models;
 
 public class Quadratic {
 
+	public double a;
+	public double b;
+	public double c;
+	
+	public Quadratic(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double getA() {
+		return a;
+	}
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+	
+	
 }

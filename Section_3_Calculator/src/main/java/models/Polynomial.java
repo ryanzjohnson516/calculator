0e82183eee198007bc0b854000cb57c702897421
Ryan <Ryan@DESKTOP-C4E4KQA>
2019-05-10 18:53:05 -0600

    Polynomial edits

--- Section_3_Calculator/src/main/java/models/Polynomial.java
+++ Section_3_Calculator/src/main/java/models/Polynomial.java
@@ -20,7 +20,7 @@ public class Polynomial {
 	
 	public double[] getPossibleNumOfRealRoots()
 	{
-		double[] NumOfRealRoots = new double[(int) (HighestExponent / 2)];
+		double[] NumOfRealRoots = new double[(int) (((HighestExponent)/2) + 1)];
 		
 		if(HighestExponent % 2 == 0) 
 		{
@@ -41,6 +41,53 @@ public class Polynomial {
 		return NumOfRealRoots;
 	}
 	
+	public double[] getPossibleNumOfImaginaryRoots()
+	{
+		double[] NumOfImaginaryRoots = new double[(int) (((HighestExponent)/2) + 1)];
+		
+		for(int i = 1; i< HighestExponent; i+=2)
+		{
+			NumOfImaginaryRoots[i] = HighestExponent;
+		}
+		
+		return NumOfImaginaryRoots;
+	}
+	
+	public double[] getNumOfPosotiveRealRoots()
+	{
+		double[] numOfPosRootsArray = new double[(int) HighestExponent];
+		double numOfPosRoots = 0;
+		for(int i = 0; i < coefficients.length; i++)
+		{
+			if(coefficients[i] < 0 && coefficients[i + 1] > 0)
+			{
+				numOfPosRoots++;
+			}
+			else if(coefficients[i] > 0 && coefficients[i + 1] < 0)
+			{
+				numOfPosRoots++;
+			}
+		}
+		
+		numOfPosRootsArray[0] = numOfPosRoots;
+		int q = 1;
+		for(int i = (int) (numOfPosRoots - 2); i > 0; i-=2)
+		{
+			numOfPosRootsArray[q] = i;
+			q++;
+		}
+		
+		return numOfPosRootsArray;
+	}
+	
+	public double[] getNumOfNegativeRealRoots()
+	{
+		double[] numOfNegRealRoots = new double[(int) HighestExponent];
+		
+		
+		
+		
+	}
 	
 	public void Print()
 	{
